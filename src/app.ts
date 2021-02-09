0c42ead4afb6021e5f6109a10a78422449a3c1d9
ChristianMcc <Christian.mccaffery@softwire.com>
2021-02-09 21:28:04 +0000

    Initial commit

---
+++ src/app.ts
@@ -0,0 +1,23 @@
+import express from "express";
+import cors from "cors";
+import "express-async-errors";
+import postRoutes from "./routes/postRoutes";
+import userRoutes from "./routes/userRoutes";
+
+const app = express();
+const port = process.env['PORT'] || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(cors({origin: "http://localhost:3000"}));
+
+app.use('/posts', postRoutes);
+app.use('/users', userRoutes);
+
+app.get('', (request, response) => {
+    return response.status(200)
+});
+
+app.listen(port, () => {
+    console.log(`Server started and is running at http://localhost:${port}`);
+});
